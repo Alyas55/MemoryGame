17b80ddf7861147c4b636aaaead691081cbc4968
Alyas55 <75093735+Alyas55@users.noreply.github.com>
2021-01-01 20:09:15 +0100

    tilføjet en Alyas kommentar

--- MemoryGame/Program.cs
+++ MemoryGame/Program.cs
@@ -8,6 +8,8 @@ namespace MemoryGame
 {
     static class Program
     {
+
+        //Alyas kommentar
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
